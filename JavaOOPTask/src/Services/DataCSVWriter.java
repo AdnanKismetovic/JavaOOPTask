6feb264c03e2d4ccb037650078073ff7dd539057
AdnanKismetovic <a.kismetovic@tntsoft.ba>
2022-10-12 10:38:50 +0200

    refactoring data writer

--- JavaOOPTask/src/Services/DataCSVWriter.java
+++ JavaOOPTask/src/Services/DataCSVWriter.java
@@ -11,35 +11,19 @@ import java.util.TreeMap;
 public class DataCSVWriter implements DataWriter{
     @Override
     public void save(TreeMap<Integer, Result> results, Logger logger) {
-        PrintWriter printWriter = null;
-        try {
+        String filepath = java.util.UUID.randomUUID() + "results.csv";
+        try (PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(filepath)))){
             logger.info("Writing results to file...");
-            String filepath = java.util.UUID.randomUUID().toString() + "results.csv";
             logger.info("File name: " + filepath);
-            logger.info("File path: " + Paths.get("").toAbsolutePath().toString());
-            File file = new File(filepath);
-            if (file.createNewFile()) {
-                logger.info("File created.");
-            } else {
-                logger.error("File already exists.");
-            }
-            FileWriter fileWriter = new FileWriter(filepath, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            printWriter = new PrintWriter(bufferedWriter);
+            logger.info("File path: " + Paths.get("").toAbsolutePath() + filepath);
             printWriter.println("id, name, min, max, avg");
             logger.info("Printing results...");
             for (Map.Entry<Integer, Result> r : results.entrySet()) {
                 printWriter.println(r.getKey() + "," + r.getValue().toString());
             }
             printWriter.flush();
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-        } finally {
-            try {
-                printWriter.close();
-            } catch (Exception ex) {
-                System.out.println("Error: " + ex.getMessage());
-            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
         }
     }
 }
